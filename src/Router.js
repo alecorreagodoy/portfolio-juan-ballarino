5754a1213dc85bae2c3b89b75930ae72a9ba0187
alecorreagodoy <alejandro.correa.godoy@gmail.com>
2020-09-20 15:25:15 +0200

    adding video

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
  import Body from './components/Body';
  import Aboutme from './components/aboutme';
